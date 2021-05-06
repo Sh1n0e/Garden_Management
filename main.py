2065790a3597ca30fd7d279d889cbe5ed22ac152
Sh1n0e <62055509+Sh1n0e@users.noreply.github.com>
2021-05-06 18:17:53 -0400

    added entries for all fields needed with databse items, need to connect soon

--- main.py
+++ main.py
@@ -65,20 +65,20 @@ class PageOne(tk.Frame):
                             command=lambda: controller.show_frame(PageTwo))
         button2.pack()
 
-        L1 = tk.Label(self, text="Plant ID")
-        L1.pack()
-        T1 = tk.Entry(self, width=30, bd=5)
-        T1.pack()
+        plant_id = tk.Label(self, text="Plant ID")
+        plant_id.pack()
+        plant_id_entry = tk.Entry(self, width=30, bd=5)
+        plant_id_entry.pack()
 
-        L2 = tk.Label(self, text="Plant Name")
-        L2.pack()
-        T2 = tk.Entry(self, width=30, bd=5)
-        T2.pack()
+        plant_name = tk.Label(self, text="Plant Name")
+        plant_name.pack()
+        plant_name_entry = tk.Entry(self, width=30, bd=5)
+        plant_name_entry.pack()
 
-        L3 = tk.Label(self, text="Harvestable?")
-        L3.pack()
-        T3 = tk.Entry(self, width=30, bd=5)
-        T3.pack()
+        harvestable = tk.Label(self, text="Harvestable?")
+        harvestable.pack()
+        harvestable_entry = tk.Entry(self, width=30, bd=5)
+        harvestable_entry.pack()
 
 
 
@@ -93,10 +93,24 @@ class PageTwo(tk.Frame):
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        button2 = tk.Button(self, text="Page One",
+        button2 = tk.Button(self, text="Visit plant page",
                             command=lambda: controller.show_frame(PageOne))
         button2.pack()
 
+        equipment_id = tk.Label(self, text="Equipment ID")
+        equipment_id.pack()
+        equipment_id_entry = tk.Entry(self, width=30, bd=5)
+        equipment_id_entry.pack()
+
+        equipment_name = tk.Label(self, text="Equipment Name")
+        equipment_name.pack()
+        equipment_name_entry = tk.Entry(self, width=30, bd=5)
+        equipment_name_entry.pack()
+
+        quantity = tk.Label(self, text="Quantity")
+        quantity.pack()
+        quantity_entry = tk.Entry(self, width=30, bd=5)
+        quantity_entry.pack()
 
 app = Gardenhub()
 app.mainloop()
