e6c6c03812296c65b58dd0d39007bf97a531ccab
Sh1n0e <62055509+Sh1n0e@users.noreply.github.com>
2021-05-09 23:37:54 -0400

    Changed for observation from Mr.Denton

--- PlantFrame.py
+++ PlantFrame.py
@@ -5,7 +5,7 @@ class Plants(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.Plant_window_label = tk.Label(self, text="Garden system: Plants")
-        self.Plant_window_label.place(relx=0.3, rely=0.2, anchor=tk.CENTER)
+        self.Plant_window_label.grid(row=0, column=1 )
 
         self.var_plant_index = tk.IntVar()
         self.var_plant_index.set(0)
@@ -15,40 +15,40 @@ class Plants(tk.Frame):
         self.var_harvest = tk.StringVar(self)
 
         self.plant_id = tk.Label(self, text="Plant ID:")
-        self.plant_id.place(relx=0.1, rely=0.3, anchor=tk.CENTER)
+        self.plant_id.grid()
         self.plant_id_entry = tk.Entry(self)
         self.entry_plant_id = tk.Entry(self, textvariable=self.var_plant_id)
-        self.entry_plant_id.place(relx=0.3, rely=0.3, anchor=tk.CENTER)
+        self.entry_plant_id.grid(row=1, column=0)
 
         self.plant_name = tk.Label(self, text="Plant name:")
-        self.plant_name.place(relx=0.1, rely=0.4, anchor=tk.CENTER)
+        self.plant_name.grid(row=2, column=-0)
         self.plant_name_entry = tk.Entry(self)
         self.entry_plant_name = tk.Entry(self, textvariable=self.var_plant_name)
-        self.entry_plant_name.place(relx=0.3, rely=0.4, anchor=tk.CENTER)
+        self.entry_plant_name.grid(row=2, column=1)
 
         self.Harvest = tk.Label(self, text="Harvestable?:")
-        self.Harvest.place(relx=0.1, rely=0.5, anchor=tk.CENTER)
+        self.Harvest.grid(row=3, column=0)
         self.Harvest_entry = tk.Entry(self)
         self.entry_Harvest = tk.Entry(self, textvariable=self.var_harvest)
-        self.entry_Harvest.place(relx=0.3, rely=0.5, anchor=tk.CENTER)
+        self.entry_Harvest.grid(row=3, column=1)
 
         self.load_plants_btn = tk.Button(self, text="Load", command=self.load_plants)
-        self.load_plants_btn.place(relx=0.3, rely=0.6, anchor=tk.CENTER)
+        self.load_plants_btn.grid(row=5, column=2)
 
         self.next_plant_btn = tk.Button(self, text=">", command=self.next_plant)
-        self.next_plant_btn.place(relx=0.4, rely=0.6, anchor=tk.CENTER)
+        self.next_plant_btn.grid(row=6, column=1)
 
         self.prev_plant_btn = tk.Button(self, text="<", command=self.prev_plant)
-        self.prev_plant_btn.place(relx=0.2, rely=0.6, anchor=tk.CENTER)
+        self.prev_plant_btn.grid(row=6, column=3)
 
         self.new_plant_btn = tk.Button(self, text="New data", command=self.new_plant)
-        self.new_plant_btn.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        self.new_plant_btn.grid(row=5, column=3)
 
         self.delete_plant_btn = tk.Button(self, text="Delete entry", command=self.delete_plant)
-        self.delete_plant_btn.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+        self.delete_plant_btn.grid(row=3, column=3)
 
         self.update_plant_btn = tk.Button(self, text="Update entry", command=self.update_plant, state="disabled")
-        self.update_plant_btn.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
+        self.update_plant_btn.grid(row=4, column=3)
 
         self.set_plant_data()
 
--- main.py
+++ main.py
@@ -6,26 +6,25 @@ LARGE_FONT = ("Verdana", 12)
 MEDIUM_FONT = ("Verdana", 10)
 
 
-
 class Gardenhub(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
 
-        container.pack(side="top", fill="both", expand=True)
+        container.grid(row=0, column=0)
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo, pf.Plants):
+        for F in (StartPage, PageOne, pf.Plants):
             frame = F(self, container)
 
             self.frames[F] = frame
 
-        self.frames[StartPage].grid(row=0, column=0, sticky="nsew")
+            self.frames[F].grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
@@ -39,90 +38,32 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Welcome to the garden", font=MEDIUM_FONT)
-        label.pack(pady=10, padx=10)
+        label.grid(row=1, column=1)
 
-        button = tk.Button(self, text="Visit Plant Page",
-                           command=lambda: controller.show_frame(PageOne))
-        button.pack()
+        button1 = tk.Button(self, text="Visit Equipment Page",
+                            command=lambda: parent.show_frame(PageOne))
+        button1.grid(row=3, column=1)
 
-        button2 = tk.Button(self, text="Visit Equipment Page",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
-
-        button3 = tk.Button(self, text="Test page",
+        button2 = tk.Button(self, text="Test page",
                             command=lambda: controller.show_frame(pf.Plants))
-        button3.pack()
-
-
+        button2.grid(row=4, column=1)
 
 
 class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Plant page!!!", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-
-        button1 = tk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = tk.Button(self, text="Visit Equipment Page",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
-
-        plant_id = tk.Label(self, text="Plant ID")
-        plant_id.pack()
-        plant_id_entry = tk.Entry(self, width=30, bd=5)
-        plant_id_entry.pack()
-
-        plant_name = tk.Label(self, text="Plant Name")
-        plant_name.pack()
-        plant_name_entry = tk.Entry(self, width=30, bd=5)
-        plant_name_entry.pack()
-
-        harvestable = tk.Label(self, text="Harvestable?")
-        harvestable.pack()
-        harvestable_entry = tk.Entry(self, width=30, bd=5)
-        harvestable_entry.pack()
-
-        next_plant = tk.Button(self, text=">")
-        next_plant.pack()
-
-        prev_plant = tk.Button(self, text="<")
-        prev_plant.pack()
-
-
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Equipment Page!!!", font=MEDIUM_FONT)
-        label.pack(pady=10, padx=10)
+        label.grid(row=1, column=1)
 
         button1 = tk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
-        button1.pack()
+        button1.grid(row=2, column=1)
 
         button2 = tk.Button(self, text="Visit plant page",
                             command=lambda: controller.show_frame(PageOne))
-        button2.pack()
+        button2.grid(row=3, column=2)
 
-        equipment_id = tk.Label(self, text="Equipment ID")
-        equipment_id.pack()
-        equipment_id_entry = tk.Entry(self, width=30, bd=5)
-        equipment_id_entry.pack()
-
-        equipment_name = tk.Label(self, text="Equipment Name")
-        equipment_name.pack()
-        equipment_name_entry = tk.Entry(self, width=30, bd=5)
-        equipment_name_entry.pack()
-
-        quantity = tk.Label(self, text="Quantity")
-        quantity.pack()
-        quantity_entry = tk.Entry(self, width=30, bd=5)
-        quantity_entry.pack()
 
 app = Gardenhub()
 app.mainloop()
